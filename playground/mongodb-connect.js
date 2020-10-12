c13896c1d59652a6baf7d37b8c787ffaf72bb8dc
akashpunagin <akash.punagin@gmail.com>
2020-10-12 16:31:36 +0530

    first commit - added playground directory

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,52 @@
+// const MongoClient = require("mongodb").MongoClient;
+const {MongoClient, ObjectID} = require("mongodb"); // NOTE: This is object destructuring
+
+// Making a new object id
+var objId = new ObjectID();
+console.log(objId);
+
+// Database name
+const dbName = "TodoApp";
+
+MongoClient.connect(
+  "mongodb://localhost:27017",
+  {useUnifiedTopology: true},
+  (err, client) => {
+    if (err) {
+      console.log("Unable to connect to MongoDB server");
+    } else {
+      console.log("Connected to MongoDB server");
+
+      // Get databasee object from client
+      var db = client.db(dbName);
+
+      // // Add data to TodoApp database
+      // db.collection("Todos").insertOne({
+      //   text: "Something to do",
+      //   completed: false,
+      // }, (err, result) => {
+      //   if (err) {
+      //     console.log("Unable to insert todo", err);
+      //   } else {
+      //     console.log(JSON.stringify(result.ops, undefined, 2));
+      //     client.close();
+      //   }
+      // });
+
+      // db.collection("Users").insertOne({
+      //   name: "Akash",
+      //   age: 20,
+      //   location: "Navanagar, Hubballi"
+      // }, (err, result) => {
+      //   if (err) {
+      //     console.log("Unable to insert todo", err);
+      //   } else {
+      //     console.log(JSON.stringify(result.ops, undefined, 2));
+      //     console.log(`Created At: ${result.ops[0]._id.getTimestamp()}`);
+      //     client.close();
+      //   }
+      // });
+
+    }
+  }
+);
---
+++ playground/mongodb-find.js
@@ -0,0 +1,55 @@
+// const MongoClient = require("mongodb").MongoClient;
+const {MongoClient, ObjectID} = require("mongodb"); // NOTE: This is object destructuring
+
+// Database name
+const dbName = "TodoApp";
+
+MongoClient.connect(
+  "mongodb://localhost:27017",
+  {useUnifiedTopology: true},
+  (err, client) => {
+    if (err) {
+      console.log("Unable to connect to MongoDB server");
+    } else {
+      console.log("Connected to MongoDB server");
+
+      // Get databasee object from client
+      var db = client.db(dbName);
+
+      // // Fetch all documents
+      // db.collection('Todos').find().toArray().then((docs) => {
+      //   console.log("Todos");
+      //   console.log(JSON.stringify(docs, undefined, 2));
+      // }, (err) => {
+      //   console.log("Unable to fetch todos", err);
+      // });
+
+      // Fetch documents by field
+      // db.collection('Todos').find({completed: false}).toArray().then((docs) => {
+      //   console.log("Todos");
+      //   console.log(JSON.stringify(docs, undefined, 2));
+      // }, (err) => {
+      //   console.log("Unable to fetch todos", err);
+      // });
+
+      // Fetch documents by ObjectID
+      // db.collection('Todos').find({
+      //   _id: new ObjectID('5f84210b1c4c682556d39bb6') ,
+      // }).toArray().then((docs) => {
+      //   console.log("Todos");
+      //   console.log(JSON.stringify(docs, undefined, 2));
+      // }, (err) => {
+      //   console.log("Unable to fetch todos", err);
+      // });
+
+      // document count
+      db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos Count: ${count}`);
+      }, (err) => {
+        console.log("Unable to fetch todos", err);
+      });
+
+
+    }
+  }
+);
